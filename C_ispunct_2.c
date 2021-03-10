273325ea42c2973cbc899ff10a3055d98a3a4b22
SEYEON_PARK <fourleaf123@naver.com>
2021-03-11 01:45:45 +0900

    C언어 <ctype.h>의 ispunct(구두점인지 아닌지 확인해준다.) 함수 연습!(2)

---
+++ C_ispunct_2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main(void)
+{
+	char a = '?';
+
+	if (ispunct(a) == 0)
+	{
+		printf("구두점이 아닙니다.\n");
+	}
+	else
+	{
+		printf("구두점입니다.\n");
+	}
+
+	return 0;
+}
